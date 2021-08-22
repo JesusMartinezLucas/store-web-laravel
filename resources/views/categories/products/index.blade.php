b63e8cd738d9d6988881a6570a9f75ea6b15c48c
Jesús Martínez Lucas <jesusmartinezlucas@gmail.com>
2021-08-22 17:33:54 -0500

    Blade components

--- resources/views/categories/products/index.blade.php
+++ resources/views/categories/products/index.blade.php
@@ -2,54 +2,23 @@
 
 @section('content')
 <div class="flex justify-center">
-    <div class="w-8/12 bg-white p-6 rounded-lg">
-        @if ($products->count())
-            @foreach ($products as $product)
-                <div class="mb-4">
-                    <a href="{{ route('categories.products', $product->category) }}" class="font-bold">{{ $product->category->name }}</a> <span
-                    class="text-gray-600 text-sm"> $ {{ $product->price }}</span>
+    <div class="w-8/12 bg-white">
+        <div class="p-6">
+            <h1 class="text-2xl font-medium mb-1">{{ $category->name }}</h1>
+            <p>{{ $products->count() }} {{ Str::plural('producto', $products->count()) }}</p>
+        </div>
 
-                    <p>{{ $product->description }}</p>
+        <div class="bg-white p-6 rounded-lg">
+            @if ($products->count())
+                @foreach ($products as $product)
+                    <x-product :product="$product" /> 
+                @endforeach
 
-                    <div>
-                        <form action="{{ route('products.destroy', $product) }}" method="post">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit" class="text-blue-500">Eliminar</button>
-                        </form>
-                    </div>
-
-                    <div class="flex items-center">
-                        @auth
-                            @if ($product->inStock())
-                                <form action="{{ route('products.sales', $product) }}" method="post" class="mr-1">
-                                    @csrf
-                                    <button type="submit" class="text-blue-500">Vender</button>
-                                </form>
-                            @endif
-                        @endauth
-                        
-                        <span class="mr-1">{{ $product->sales->count() }} {{ Str::plural('vendido', 
-                        $product->sales->count()) }}</span>
-
-                        @auth 
-                            @can('deleteLastSale', $product)
-                                <form action="{{ route('products.sales', $product) }}" method="post">
-                                    @csrf 
-                                    @method('DELETE')
-                                    <button type="submit" class="text-blue-500">Devolver última venta</button>
-                                </form>
-                            @endcan
-                        @endauth
-                    </div>
-
-                </div>
-            @endforeach
-
-            {{ $products->links() }}
-        @else
-            <p>No hay productos</p>
-        @endif
+                {{ $products->links() }}
+            @else
+                <p>La {{ $category->name }} no tiene productos</p>
+            @endif
+        </div>
     </div>
 </div>
 @endsection
--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -67,45 +67,7 @@
 
         @if ($products->count())
             @foreach ($products as $product)
-                <div class="mb-4">
-                    <a href="{{ route('categories.products', $product->category) }}" class="font-bold">{{ $product->category->name }}</a> <span
-                    class="text-gray-600 text-sm"> $ {{ $product->price }}</span>
-
-                    <p>{{ $product->description }}</p>
-
-                    <div>
-                        <form action="{{ route('products.destroy', $product) }}" method="post">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit" class="text-blue-500">Eliminar</button>
-                        </form>
-                    </div>
-
-                    <div class="flex items-center">
-                        @auth
-                            @if ($product->inStock())
-                                <form action="{{ route('products.sales', $product) }}" method="post" class="mr-1">
-                                    @csrf
-                                    <button type="submit" class="text-blue-500">Vender</button>
-                                </form>
-                            @endif
-                        @endauth
-                        
-                        <span class="mr-1">{{ $product->sales->count() }} {{ Str::plural('vendido', 
-                        $product->sales->count()) }}</span>
-
-                        @auth 
-                            @can('deleteLastSale', $product)
-                                <form action="{{ route('products.sales', $product) }}" method="post">
-                                    @csrf 
-                                    @method('DELETE')
-                                    <button type="submit" class="text-blue-500">Devolver última venta</button>
-                                </form>
-                            @endcan
-                        @endauth
-                    </div>
-
-                </div>
+                <x-product :product="$product" /> 
             @endforeach
 
             {{ $products->links() }}
