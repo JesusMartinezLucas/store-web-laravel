d0356222a617d34b2316bc76179834a3f7a3b1c8
Jesús Martínez Lucas <jesusmartinezlucas@gmail.com>
2021-08-28 21:56:07 -0500

    Sending email

--- app/Http/Controllers/ProductSaleController.php
+++ app/Http/Controllers/ProductSaleController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\Product;
+use App\Mail\SaleReturned;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class ProductSaleController extends Controller
 {
@@ -33,8 +35,11 @@ class ProductSaleController extends Controller
         $this->authorize('deleteLastSale', $product);
 
         $request->user()->sales()->where('product_id', $product->id)->orderBy('id', 'desc')->first()->delete();
+
         $product->quantity += 1;
         $product->update(); 
+
+        Mail::to(auth()->user())->send(new SaleReturned($product));
 
         return back();
     }
