c36c99fae7ef67ef6ef9b65a4fe52cef8e6cf264
Jesús Martínez Lucas <jesusmartinezlucas@gmail.com>
2021-10-17 18:33:50 -0500

    Se elimina un usuario

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,7 @@ Route::get('/users/create', [UserController::class, 'create'])->name('users.crea
 Route::post('/users', [UserController::class, 'store'])->name('users.store');
 Route::get('/users/{user}/edit', [UserController::class, 'edit'])->name('users.edit');
 Route::put('/users/{user}', [UserController::class, 'update'])->name('users.update');
+Route::delete('/users/{user}', [UserController::class, 'destroy'])->name('users.destroy');
 
 Route::get('/categories/{category:name}/products', [CategoryProductController::class, 'index'])->name('categories.products');
 
