e4a6f206a82e33677b85a48b2b8926bb0af2faa0
Jesús Martínez Lucas <jesusmartinezlucas@gmail.com>
2021-10-20 19:14:37 -0500

    Se ajusta el tamaño responsivo del listado de categorías

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="flex justify-center">
         
-        <div class="w-full md:w-8/12 bg-white m-6 p-6 rounded-lg">
+        <div class="w-full sm:w-8/12 md:w-7/12 lg:w-6/12 xl:w-5/12 2xl:w-4/12 bg-white m-6 p-6 rounded-lg">
             @foreach ($categories as $category)
             <div class="mb-4">
                 <div class="flex justify-between flex-wrap">
