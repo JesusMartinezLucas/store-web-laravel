790aa64c1166b20a61a66f4029c19d762a08017a
Jesús Martínez Lucas <jesusmartinezlucas@gmail.com>
2021-10-31 13:19:52 -0600

    Se mueve el buscador al la sección de navegación

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -10,7 +10,7 @@ class ProductController extends Controller
 {
     public function __construct()
     {
-        $this->middleware(['auth'])->except(['index', 'show']);
+        $this->middleware(['auth'])->except(['index', 'show', 'search']);
     }
 
     public function index()
--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,25 +1,23 @@
 @extends('layouts.app')
 
+@section('search')
+<form action="{{ route('products.search') }}" method="GET" class="mr-2">
+    <label for="search" class="sr-only">Buscar</label>
+    <input type="text" name="search" id="search" placeholder="Buscar ..."
+    class="bg-gray-100 border-2 w-full px-4 py-2 rounded-lg @error('search')
+    border-red-500 @enderror" value="{{ old('search', isset($search) ? $search : '') }}">
+
+    @error('search')
+        <div class="text-red-500 mt-2 text-sm">
+            {{$message}}
+        </div>
+    @enderror
+</form>
+@endsection
+
 @section('content')
 <div class="flex justify-center">
     <div class="w-full flex flex-wrap justify-center md:justify-between bg-white p-6 rounded-lg">
-
-
-        <form action="{{ route('products.search') }}" method="GET">
-            <div class="mb-4">
-                <label for="search" class="sr-only">Buscar</label>
-                <input type="text" name="search" id="search" placeholder="Buscar ..."
-                class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('search')
-                border-red-500 @enderror" value="{{ old('search', isset($search) ? $search : '') }}">
-
-                @error('search')
-                    <div class="text-red-500 mt-2 text-sm">
-                        {{$message}}
-                    </div>
-                @enderror
-            </div>
-        </form>
-
 
         @if ($products->count())
             @foreach ($products as $product)
