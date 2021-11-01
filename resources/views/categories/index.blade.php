d3c18bb8639cb3c852ae9e9cf8e248c69a5e99fe
Jesús Martínez Lucas <jesusmartinezlucas@gmail.com>
2021-11-01 08:46:57 -0600

    Plus icon component

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -29,7 +29,7 @@
         </div>
 
         <a class="fixed right-8 bottom-8 sm:right-16 sm:bottom-16 rounded-full bg-blue-500 p-2" href="{{ route('categories.create') }}">
-            <svg class="inline text-white w-8 h-8" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path></svg>
+            <x-icons.plus />
         </a>
 
     </div>
