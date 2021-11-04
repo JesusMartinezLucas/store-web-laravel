3ed5b9bc2bb4ff49af2d8edd3de94349535989bc
Jesús Martínez Lucas <jesusmartinezlucas@gmail.com>
2021-11-04 08:00:05 -0600

    Products component

---
+++ resources/views/components/products.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.app')
+
+@section('search')
+    <x-search :route="$searchRoute" :search="$search" />
+@endsection
+
+@section('content')
+<div class="flex justify-center">
+    <div class="w-full flex flex-wrap justify-center md:justify-between bg-white p-6 rounded-lg">
+
+        @if ($products->count())
+            @foreach ($products as $product)
+                <x-product :product="$product" /> 
+            @endforeach
+
+            {{ $products->links() }}
+        @else
+            <p>No hay productos</p>
+        @endif
+        
+    </div>
+
+    @auth
+    <x-links.create :route="route('products.create')">
+        <x-icons.plus />
+    </x-links.create>
+    @endauth
+</div>
+@endsection
--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,29 +1,5 @@
-@extends('layouts.app')
-
-@section('search')
-    <x-search :route="route('products.search')" :search="isset($search) ? $search : ''" />
-@endsection
-
-@section('content')
-<div class="flex justify-center">
-    <div class="w-full flex flex-wrap justify-center md:justify-between bg-white p-6 rounded-lg">
-
-        @if ($products->count())
-            @foreach ($products as $product)
-                <x-product :product="$product" /> 
-            @endforeach
-
-            {{ $products->links() }}
-        @else
-            <p>No hay productos</p>
-        @endif
-        
-    </div>
-
-    @auth
-    <x-links.create :route="route('products.create')">
-        <x-icons.plus />
-    </x-links.create>
-    @endauth
-</div>
-@endsection
+<x-products 
+    :products="$products" 
+    :search="isset($search) ? $search : ''" 
+    :searchRoute="route('products.search')" 
+/>
