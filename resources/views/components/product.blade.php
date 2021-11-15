5fdf3d30b47cf996858af4eb922a8f3c33796777
Jesús Martínez Lucas <jesusmartinezlucas@gmail.com>
2021-11-15 15:15:49 -0600

    Products size

--- resources/views/components/product.blade.php
+++ resources/views/components/product.blade.php
@@ -1,4 +1,4 @@
-<div class="max-w-sm rounded overflow-hidden shadow-lg mb-6">
+<div class="w-96 rounded overflow-hidden shadow-lg flex flex-col justify-between mb-6 mx-2">
     <img class="w-full" src="/storage/image/{{ !is_null($product->image) ? $product->image : 'noImage.jpeg' }}" alt="Imagen del producto">
     <div class="p-4">
         <div class="flex flex-wrap justify-start">
