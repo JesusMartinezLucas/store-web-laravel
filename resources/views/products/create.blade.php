5285748ee21a1e1df0d7869cced107fa264dfa7a
Jesús Martínez Lucas <jesusmartinezlucas@gmail.com>
2021-12-07 09:00:22 -0600

    Play video and grab a snapshot

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -99,6 +99,38 @@
                 font-medium w-full md:w-1/4">Guardar producto</button>
             </div>
         </form>
+
+        <video id="player" controls autoplay></video>
+        <button id="capture">Capture</button>
+        <canvas id="canvas" width=320 height=240></canvas>
+
     </div>
 </div>
+
+<script>
+  const player = document.getElementById('player');
+  const canvas = document.getElementById('canvas');
+  const context = canvas.getContext('2d');
+  const captureButton = document.getElementById('capture');
+
+  const constraints = {
+    video: true,
+  };
+
+  captureButton.addEventListener('click', () => {
+    // Draw the video frame to the canvas.
+    context.drawImage(player, 0, 0, canvas.width, canvas.height);
+
+    console.log("player ", player);
+    console.log("canvas ", canvas);
+    console.log("context ", context);
+  });
+
+  // Attach the video stream to the video element and autoplay.
+  navigator.mediaDevices.getUserMedia(constraints)
+    .then((stream) => {
+      player.srcObject = stream;
+    });
+</script>
+
 @endsection
